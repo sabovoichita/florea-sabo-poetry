8603dcee4bb310342efbf88286dde9adfaef6978
Voichita Maria <sabo_voichita@yahoo.com>
2024-04-25 18:31:33 +0200

    poetry 8.json

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function loadPoetries(poetryNumber) {
     });
 }
 function initEvents() {
-  const numberOfLessons = 7;
+  const numberOfLessons = 8;
   for (let i = 1; i <= numberOfLessons; i++) {
     loadPoetries(i);
   }
