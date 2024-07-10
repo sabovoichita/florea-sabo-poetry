b26b031930bfd516e3438e1a4370095bedb6a6d2
Voichita Maria <sabo_voichita@yahoo.com>
2024-07-10 16:01:31 +0200

    20.json

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function showPoem(index) {
 }
 
 function initEvents() {
-  const numberOfPoems = 19;
+  const numberOfPoems = 20;
   for (let i = 1; i <= numberOfPoems; i++) {
     loadPoetries(i);
   }
